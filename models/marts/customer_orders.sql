aca54f875864656b3243805423a1531a8c662e80
Kokila K S <kskokilagowda@gmail.com>
2026-01-09 15:17:04 +0000

    duplicated

---
+++ models/marts/customer_orders.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+select
+    c.CustomerID,
+    c.CustomerName,
+    c.City,
+    count(o.OrderID) as total_orders,
+    sum(o.Amount) as total_amount
+from {{ source('sf_raw', 'Customer') }} c
+left join {{ source('sf_raw', 'Orders') }} o
+    on c.CustomerID = o.CustomerID
+group by 1,2,3
