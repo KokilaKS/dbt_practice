a9b639b8a510b22002ba48d4d87cf2f7f8c8e208
Kokila K S <kskokilagowda@gmail.com>
2026-01-13 15:43:13 +0000

    orders changed

--- dbt_practice/models/marts/customer_orders.sql
+++ dbt_practice/models/marts/customer_orders.sql
@@ -7,6 +7,6 @@ select
     count(o.OrderID) as total_orders,
     sum(o.Amount) as total_amount
 from {{ source('sf_raw', 'customers') }} c
-left join {{ source('sf_raw', 'Orders') }} o
+left join {{ source('sf_raw', 'orders') }} o
     on c.CustomerID = o.CustomerID
 group by 1,2,3
